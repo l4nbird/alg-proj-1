910414344ed292dec249badd7e76c84fc5042a61
Erik Stinnett <estinnett@cub.uca.edu>
2022-09-20 23:21:03 -0500

    Prime number generator progress

--- primegenerator.py
+++ primegenerator.py
@@ -2,6 +2,7 @@
 
 import math
 import random
+from tkinter import N
 
 
 # Fermat's Little Theorem: If n = prime integer and a = positive integer less than n, then
@@ -15,29 +16,36 @@ import random
 
 # 1) Generate n
 # 2) Generate a (a E [1, n-1])
-# 3) Test if n is prime (a^n % n = a)
+# 3) Test if n is prime (a ** (n-1)) % n != 1
 # 4) if YES, save n as p or q (Return true)
 # 5) if no, return false (repeat algorithm, new int)
 
-p = 0   # First prime number
+
+p = -1
+q = -1
+primeNumber = 0
 
 def get_prime_number():
-    min = 2
+    min = 21
     #infinity = math.inf
 
-    test = False
+    test = True
 
     n = random.randint(min, 100)   # Generate n between 2 and infinity : Check if infinity is necessary!!!  1,000,000 to 10,000,000
                                     # check only 20 times? pseudoprime
     a = random.randint(2, (n-1))        # Generate 1 < a < n
     
-    for i in range(1, n):
-        if (a**n) % n != a:
-            test = False
-
-        if test == False:
-            print("Not prime")
-        else:
-            print("Prime")
     print("n = " + str(n) + " a = " + str(a))
-get_prime_number()
+
+    for i in range(1, 3):
+        if (a ** (n-1)) % n != 1:   # If != 1, then NOT prime
+            test = False
+            break
+    if not test:
+        print("Not prime")
+    else:
+        print("prime")
+        return p, q
+
+p, q = get_prime_number()
+#print("p = " + str(p))
