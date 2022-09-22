ddebd7a26c4dfe617e52d7b8e5a706f3014f92a9
Erik Stinnett <estinnett@cub.uca.edu>
2022-09-22 10:34:42 -0500

    Updated prime generator

--- primegenerator.py
+++ primegenerator.py
@@ -3,6 +3,7 @@
 import math
 import random
 from tkinter import N
+from webbrowser import get
 
 
 # Fermat's Little Theorem: If n = prime integer and a = positive integer less than n, then
@@ -15,34 +16,50 @@ from tkinter import N
 # 4) if YES, save n as p or q (Return true)
 # 5) if no, return false (repeat algorithm, new int)
 
-def get_prime_number():
-    min = 21
-    #infinity = math.inf
 
-    test = True
+# A function to generate a list of 20 potential prime numbers and 20 A's
+def get_numbers(x = 1000000):  # n = 1,000,000
 
-    n = random.randint(min, 100)   # Generate n between 2 and infinity : Check if infinity is necessary!!!  1,000,000 to 10,000,000
-                                    # check only 20 times? pseudoprime
-    a = random.randint(2, (n-1))        # Generate 1 < a < n
-    
-    print("n = " + str(n) + " a = " + str(a))
+    # List initializing
+    primeNumList = []           
+    a_list = []
+    n = random.randint(x, 10*x) # Get random integer between 1,000,000 and 10n
 
-    if (n % 2 == 0):
-        return(get_prime_number())
-    else:
-        for i in range(1, 3):
-            if (a ** (n-1)) % n != 1:   # If != 1, then NOT prime
-                test = False
-                break
-        if test == False:
-            print("Not prime")
-            return(get_prime_number())
+
+    while len(primeNumList) < 20:
+        if (n % 2) != 0 and (n % 3) != 0 and (n % 5) != 0:  # Quickens the process, removes known factors 
+            primeNumList.append(n)                          # If it passes the parameters, add it to the primeNumList
+            a = random.randint(2, n-1)                      # generates a
+            a_list.append(a)
+            n = random.randint(x, 10*x)                     # Generate new rand int
+
         else:
-            print("prime")
-            primeNumber = n
-            return primeNumber
-        
+            n = random.randint(x, 10*x)                     # Generate new rand int
 
-#primeNumber = get_prime_number()
-#p = primeNumber
-#print("p = " + str(p))
+    return primeNumList, a_list
+
+
+def test_prime_number():
+    min = 2
+    test = True
+    prime = []
+    for (a, b) in zip(a_list, primeNumList):                # Fermat's primality test
+        if pow(a, b-1, b) == 1:   # If != 1, then NOT prime
+            prime.append(b)
+
+    return prime
+
+
+def get_p_and_q():      # Get p and q from prime list
+    p = prime.pop()
+    q = prime.pop()
+    
+    return p, q
+
+
+
+primeNumList, a_list = get_numbers()
+prime = test_prime_number()
+p, q = get_p_and_q()
+
+
